c1bcbfa269de2948588190a4a373a609c0415943
dirocca <diroccagsm@gmail.com>
2013-09-10 18:51:36 +0200

    add picons(1-10)

--- picons.sh
+++ picons.sh
@@ -1,3 +1,61 @@
-# vrt_radio1
+cd/picons
 
-ln -s vrt_radio.png 1_0_2_31EA_45F_35_C00000_0_0_0.png 
+#vrt_radio1
+
+ln -s vrt_radio1.png1_0_2_31EA_45F_35_C00000_0_0_0.png 
+
+
+
+
+#vrt_radio2
+
+ln -s vrt_radio2.png1_0_2_31EB_45F_35_C00000_0_0_0.png
+
+
+
+#bnr_nieuwsradio
+
+ln -s bnr_nieuwsradio.png1_0_2_5192_C8B_3_EB0000_0_0_0.png
+
+
+
+#vrt_studio_brussel
+
+ln -s vrt_studio_brussel.png1_0_2_31ED_45F_35_C00000_0_0_0.png
+
+
+
+#classic_fm_nl
+
+
+ln -s classic_fm_nl.png1_0_2_518A_C8B_3_EB0000_0_0_0.png
+
+
+#joe_fm_be
+
+
+ln -s joe_fm_be.png1_0_2_31F3_45F_35_C00000_0_0_0.png
+
+
+#nostalgie_be
+
+
+ln -s nostalgie_be.png1_0_2_31F5_45F_35_C00000_0_0_0.png
+
+
+
+#q_music_be
+
+ln -s q_music_be.png1_0_2_31F2_45F_35_C00000_0_0_0.png
+
+
+
+#vrt_klara_belgium
+
+ln -s vrt_klara_belgium.png1_0_2_31EE_45F_35_C00000_0_0_0.png
+
+
+
+#vrt_mnm
+
+ln -s vrt_mnm.png1_0_2_31EC_45F_35_C00000_0_0_0.png
