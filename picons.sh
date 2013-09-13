344967896413d65a039428878f4ad46165ef9797
dirocca <diroccagsm@gmail.com>
2013-09-13 16:15:47 +0200

    add all picons for TVV-CD

--- picons.sh
+++ picons.sh
@@ -1,61 +1,697 @@
 cd/picons
 
-#vrt_radio1
+# vrt_radio1
 
 ln -s vrt_radio1.png1_0_2_31EA_45F_35_C00000_0_0_0.png 
 
 
 
 
-#vrt_radio2
+# vrt_radio2
 
 ln -s vrt_radio2.png1_0_2_31EB_45F_35_C00000_0_0_0.png
 
 
 
-#bnr_nieuwsradio
+# bnr_nieuwsradio
 
 ln -s bnr_nieuwsradio.png1_0_2_5192_C8B_3_EB0000_0_0_0.png
 
 
 
-#vrt_studio_brussel
+# vrt_studio_brussel
 
 ln -s vrt_studio_brussel.png1_0_2_31ED_45F_35_C00000_0_0_0.png
 
 
 
-#classic_fm_nl
+# classic_fm_nl
 
 
 ln -s classic_fm_nl.png1_0_2_518A_C8B_3_EB0000_0_0_0.png
 
 
-#joe_fm_be
+# joe_fm_be
 
 
 ln -s joe_fm_be.png1_0_2_31F3_45F_35_C00000_0_0_0.png
 
 
-#nostalgie_be
+# nostalgie_be
 
 
 ln -s nostalgie_be.png1_0_2_31F5_45F_35_C00000_0_0_0.png
 
 
 
-#q_music_be
+# q_music_be
 
 ln -s q_music_be.png1_0_2_31F2_45F_35_C00000_0_0_0.png
 
 
 
-#vrt_klara_belgium
+# vrt_klara_belgium
 
 ln -s vrt_klara_belgium.png1_0_2_31EE_45F_35_C00000_0_0_0.png
 
 
 
-#vrt_mnm
+# vrt_mnm
 
 ln -s vrt_mnm.png1_0_2_31EC_45F_35_C00000_0_0_0.png
+
+
+
+# 100%_nl
+
+ln -s 100%_nl.png1_0_2_519A_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# arrow_classic_rock
+
+ln -s arrow_classic_rock.png1_0_2_519C_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# arrow_jazz_nl
+
+ln -s arrow_jazz_nl.png1_0_2_519E_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# fun_x
+
+ln -s fun_x.png1_0_2_14A7_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio_10_gold_nl
+
+ln -s radio_10_gold_nl.png1_0_2_518E_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# radio_538_nl
+
+ln -s radio_538_nl.png1_0_2_518C_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# rtl_1440
+
+ln -s rtl_1440.png1_0_2_1498_40D_1_C00000_0_0_0.png
+
+
+
+
+# rtl_radio
+
+ln -s rtl_radio.png1_0_2_1497_40D_1_C00000_0_0_0.png
+
+
+
+
+# sky_radio
+
+ln -s sky_radio.png1_0_2_5188_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# veronica_radio
+
+ln -s veronica_radio.png1_0_2_5186_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# beat_m
+
+ln -s beat_m.png1_0_2_335F_C93_3_EB0000_0_0_0.png
+
+
+
+
+# country_radio
+
+ln -s country_radio.png1_0_2_335E_C93_3_EB0000_0_0_0.png
+
+
+
+
+# radio1_nl
+
+ln -s radio1_nl.png1_0_2_14A1_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio2_nl
+
+ln -s radio2_nl.png1_0_2_14A2_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio3_fm_nl
+
+ln -s radio3_fm_nl.png1_0_2_14A3_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio4_nl
+
+ln -s radio4_nl.png1_0_2_14A4_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio5_nl
+
+ln -s radio5_nl.png1_0_2_14A5_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio6_nl
+
+ln -s radio6_nl.png1_0_2_14A6_40D_1_C00000_0_0_0.png
+
+
+
+
+# radio_beat
+
+ln -s radio_beat.png1_0_2_3340_C89_3_EB0000_0_0_0.png
+
+
+
+
+# rtv_gelderland_radio
+
+ln -s rtv_gelderland_radio.png1_0_2_516E_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# bbc_arabic
+
+ln -s bbc_arabic.png1_0_2_216F_400_1_C00000_0_0_0.png
+
+
+
+
+# bbc_cz
+
+ln -s bbc_cz.png1_0_2_1F58_BC6_3_EB0000_0_0_0.png
+
+
+
+
+# bbc_world_service
+
+ln -s bbc_world_service.png1_0_2_2175_400_1_C00000_0_0_0.png
+
+
+
+
+# cro_plus
+
+ln -s cro_plus.png1_0_2_1F5B_BC6_3_EB0000_0_0_0.png
+
+
+
+
+# cro_radiozumal
+
+ln -s cro_radiozumal.png1_0_2_1F52_BC6_3_EB0000_0_0_0.png
+
+
+
+
+# radio_oost
+
+ln -s radio_oost.png1_0_2_516C_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# radio_wave
+
+ln -s radio_wave.png1_0_2_1F5C_BC6_3_EB0000_0_0_0.png
+
+
+
+
+# rtl2_radio
+
+ln -s rtl2_radio.png1_0_2_215A_400_1_C00000_0_0_0.png
+
+
+
+
+# rtl_radio
+
+ln -s rtl_radio.png1_0_2_2150_400_1_C00000_0_0_0.png
+
+
+
+
+# slam_fm
+
+ln -s slam_fm.png1_0_2_5190_C8B_3_EB0000_0_0_0.png
+
+
+
+
+# cherie_fm
+
+ln -s cherie_fm.png1_0_2_2164_400_1_C00000_0_0_0.png
+
+
+
+
+# europe1
+
+ln -s europe1.png1_0_2_214E_400_1_C00000_0_0_0.png
+
+
+
+
+# france_culture
+
+ln -s france_culture.png1_0_2_2173_400_1_C00000_0_0_0.png
+
+
+
+
+# fun_radio
+
+ln -s fun_radio.png1_0_2_215C_400_1_C00000_0_0_0.png
+
+
+
+
+# nrj
+
+ln -s nrj.png1_0_2_2158_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_classique
+
+ln -s radio_classique.png1_0_2_214D_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_nova
+
+ln -s radio_nova.png1_0_2_215E_400_1_C00000_0_0_0.png
+
+
+
+
+# rfm_radio
+
+ln -s rfm_radio.png1_0_2_2163_400_1_C00000_0_0_0.png
+
+
+
+
+# skyrock
+
+ln -s skyrock.png1_0_2_215B_400_1_C00000_0_0_0.png
+
+
+
+
+# virgin_radio
+
+ln -s virgin_radio.png1_0_2_2159_400_1_C00000_0_0_0.png
+
+
+
+
+# cro_radio_praha
+
+ln -s cro_radio_praha.png1_0_2_1F5D_BC6_3_EB0000_0_0_0.png
+
+
+
+
+# france_blue
+
+ln -s france_blue.png1_0_2_214B_400_1_C00000_0_0_0.png
+
+
+
+
+# france_info
+
+ln -s france_info.png1_0_2_2149_400_1_C00000_0_0_0.png
+
+
+
+
+# france_inter
+
+ln -s france_inter.png1_0_2_214A_400_1_C00000_0_0_0.png
+
+
+
+
+# france_musique
+
+ln -s france_musique.png1_0_2_2147_400_1_C00000_0_0_0.png
+
+
+
+
+# rfi_int
+
+ln -s rfi_int.png1_0_2_214C_400_1_C00000_0_0_0.png
+
+
+
+
+# rire_chansons
+
+ln -s rire_chansons.png1_0_2_2151_400_1_C00000_0_0_0.png
+
+
+
+
+# rmc
+
+ln -s rmc.png1_0_2_214F_400_1_C00000_0_0_0.png
+
+
+
+
+# rmc_doualiva
+
+ln -s rmc_doualiva.png1_0_2_2153_400_1_C00000_0_0_0.png
+
+
+
+# tsf_radio
+
+ln -s tsf_radio.png1_0_2_2154_400_1_C00000_0_0_0.png
+
+
+
+
+# ado_fm
+
+ln -s ado_fm.png1_0_2_2168_400_1_C00000_0_0_0.png
+
+
+
+
+# bfm
+
+ln -s bfm.png1_0_2_2156_400_1_C00000_0_0_0.png
+
+
+
+
+# contact_fm
+
+ln -s contact_fm.png1_0_2_2161_400_1_C00000_0_0_0.png
+
+
+
+
+# le_mouve
+
+ln -s le_mouve.png1_0_2_215D_400_1_C00000_0_0_0.png
+
+
+
+
+# oui
+
+ln -s oui.png1_0_2_2167_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_fg_fr
+
+ln -s radio_fg_fr.png1_0_2_215F_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_latina
+
+ln -s radio_latina.png1_0_2_2162_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_notredame
+
+ln -s radio_notredame.png1_0_2_216B_400_1_C00000_0_0_0.png
+
+
+
+
+# vibration
+
+ln -s vibration.png1_0_2_2160_400_1_C00000_0_0_0.png
+
+
+
+
+# voltage
+
+ln -s voltage.png1_0_2_2166_400_1_C00000_0_0_0.png
+
+
+
+
+# beur_fm
+
+ln -s beur_fm.png1_0_2_216E_400_1_C00000_0_0_0.png
+
+
+
+
+# frequence
+
+ln -s frequence.png1_0_2_2157_400_1_C00000_0_0_0.png
+
+
+
+
+# la_premiere
+
+ln -s la_premiere.png1_0_2_3241_45F_35_C00000_0_0_0.png
+
+
+
+
+# music
+
+ln -s music.png1_0_2_3240_45F_35_C00000_0_0_0.png
+
+
+
+
+# nostalgie
+
+ln -s nostalgie.png1_0_2_2155_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_alfa_fr
+
+ln -s radio_alfa_fr.png1_0_2_216C_400_1_C00000_0_0_0.png
+
+
+
+
+# radio_france
+
+ln -s radio_france.png1_0_2_2188_400_1_C00000_0_0_0.png
+
+
+
+ 
+# rfi_multliingues
+
+ln -s rfi_multliingues.png1_0_2_2170_400_1_C00000_0_0_0.png
+
+
+
+
+# sud_radio
+
+ln -s sud_radio.png1_0_2_2152_400_1_C00000_0_0_0.png
+
+
+
+
+# viva_cite
+
+ln -s viva_cite.png1_0_2_323F_45F_35_C00000_0_0_0.png
+
+
+
+# antenne_bayern
+
+ln -s antenne_bayern.png1_0_2_AA_7_85_C00000_0_0_0.png
+
+
+
+
+# bel_rtl
+
+ln -s bel_rtl.png1_0_2_3245_45F_35_C00000_0_0_0.png
+
+
+
+
+# classic_21
+
+ln -s classic_21.png1_0_2_323E_45F_35_C00000_0_0_0.png
+
+
+
+
+# fip
+
+ln -s fip.png1_0_2_2148_400_1_C00000_0_0_0.png
+
+
+
+
+# mint
+
+ln -s mint.png1_0_2_3244_45F_35_C00000_0_0_0.png
+
+
+
+
+# ndr_blue
+
+ln -s ndr_blue.png1_0_2_6F1E_445_1_C00000_0_0_0.png
+
+
+
+
+# purefm
+
+ln -s purefm.png1_0_2_323D_45F_35_C00000_0_0_0.png
+
+
+
+
+# radio_contact
+
+ln -s radio_contact.png1_0_2_3246_45F_35_C00000_0_0_0.png
+
+
+
+
+# wdr_event
+
+ln -s wdr_event.png1_0_2_6F43_445_1_C00000_0_0_0.png
+
+
+
+
+# wrn
+
+ln -s wrn.png1_0_2_3170_459_1_C00000_0_0_0.png
+
+
+
+# bayern1
+
+ln -s bayern1.png1_0_2_6EF0_445_1_C00000_0_0_0.png
+
+
+
+
+# bayern2
+
+ln -s bayern2.png1_0_2_6EF1_445_1_C00000_0_0_0.png
+
+
+
+
+# bayern3
+
+ln -s bayern3.png1_0_2_6EF2_445_1_C00000_0_0_0.png
+
+
+
+
+# bayern4_klassik
+
+ln -s bayern4_klassik.png1_0_2_6EF3_445_1_C00000_0_0_0.png
+
+
+
+
+# bayern5_aktuell
+
+ln -s bayern5_aktuell.png1_0_2_6EF4_445_1_C00000_0_0_0.png
+
+
+
+
+# bayern_plus
+
+ln -s bayern_plus.png1_0_2_6EF5_445_1_C00000_0_0_0.png
+
+
+
+
+# br_verkerhskanal
+
+ln -s br_verkerhskanal.png1_0_2_6EF7_445_1_C00000_0_0_0.png
+
+
+
+
+# hr_1
+
+ln -s hr_1.png1_0_2_6F03_445_1_C00000_0_0_0.png
+
+
+
+
+# hr_2
+
+ln -s hr_2.png1_0_2_6F04_445_1_C00000_0_0_0.png
+
+
+
+
+# on_3_radio
+
+ln -s on_3_radio.png1_0_2_6EF6_445_1_C00000_0_0_0.png
+
+
+
+
+
+
+
+
+
+
